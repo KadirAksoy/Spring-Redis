8248c02827e6d2a320239369306dbc08dfb1d600
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2023-05-25 16:41:11 +0300

    service  added

--- src/main/java/com/kadiraksoy/SpringRedis/service/EmployeeService.java
+++ src/main/java/com/kadiraksoy/SpringRedis/service/EmployeeService.java
@@ -1,27 +1,72 @@
 package com.kadiraksoy.SpringRedis.service;
 
 
+import com.kadiraksoy.SpringRedis.dto.EmployeeDto;
+import com.kadiraksoy.SpringRedis.dto.converter.Converter;
 import com.kadiraksoy.SpringRedis.model.Employee;
 import com.kadiraksoy.SpringRedis.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
+    private final Converter converter;
 
-    public EmployeeService(EmployeeRepository employeeRepository) {
+    public EmployeeService(EmployeeRepository employeeRepository, Converter converter) {
         this.employeeRepository = employeeRepository;
+        this.converter = converter;
     }
-    public List<Employee> findAll() {
+    public List<EmployeeDto> findAll() {
 
-        List<Employee> employees = new ArrayList<>();
-        employeeRepository.findAll().forEach(employees::add);
-        return employees;
+        List<Employee> employees = (List<Employee>) employeeRepository.findAll();
+        List<EmployeeDto> employeeDtos = new ArrayList<>();
+        for (Employee employee: employees) {
+            employeeDtos.add(converter.convert(employee));
+        }
+        return employeeDtos;
     }
+
+    public EmployeeDto findById(Long id) {
+        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+        return optionalEmployee.map(converter::convert).orElseThrow();
+    }
+
+    public void save(EmployeeDto employeeDto) {
+        Employee employee = new Employee();
+        employee.setId(employeeDto.getId());
+        employee.setFirstName(employeeDto.getFirstName());
+        employee.setLastName(employeeDto.getLastName());
+        employee.setPhone(employeeDto.getPhone());
+        employee.setEmail(employeeDto.getEmail());
+
+        employeeRepository.save(employee);
+    }
+    public EmployeeDto update(Long id, EmployeeDto employeeDto){
+        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+
+        optionalEmployee.ifPresent(employee -> {
+            employee.setId(employeeDto.getId());
+            employee.setFirstName(employeeDto.getFirstName());
+            employee.setLastName(employeeDto.getLastName());
+            employee.setPhone(employeeDto.getPhone());
+            employee.setEmail(employeeDto.getEmail());
+        });
+        return employeeDto;
+
+    }
+    public void deleteEmployeeByID(Long id) {
+        Optional<Employee> employee = employeeRepository.findById(id);
+        if(employee.isPresent()) {
+            Employee deletedEmployee = employee.get();
+            employeeRepository.delete(deletedEmployee);
+        }
+    }
+
 
 
 }
